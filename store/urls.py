18b4462b7d88ae5aad2c33d29735b051d30e9c65
Dmytro Zuiev <efimdmt@gmail.com>
2020-08-28 16:22:01 +0300

    Editing and detailed viewing of pictures were added

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from store.views import ListPicturesView, AddPictureView
+from store.views import ListPicturesView, AddPictureView, EditPictureView, PictureDetailsView
 
 urlpatterns = [
     path('', ListPicturesView.as_view()),
     path('add/', AddPictureView.as_view()),
-    # delete, edit, filter
+    path('edit/<int:picture_id>/', EditPictureView.as_view()),
+    path('picture/<int:picture_id>/', PictureDetailsView.as_view()),
+    # delete, filter
 ]
--- store/views/__init__.py
+++ store/views/__init__.py
@@ -1,3 +1,4 @@
+from django.http import Http404, JsonResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from store.models import Picture
@@ -23,14 +24,49 @@ class AddPictureView(TemplateView):
         form = PictureForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            next_form = PictureForm()
-            return render(request, self.template_name, {
-                'form': next_form,
-                'status': 'successful'
-            })
+            return JsonResponse({'status': 'successful'})
 
-        return render(request, self.template_name, {
-            'form': form,
+        return JsonResponse({
             'status': 'failed',
             'errors': form.errors
         })
+
+
+class PictureDetailsView(TemplateView):
+    template_name = 'picture_details.html'
+
+    def get(self, request, picture_id=-1, *args, **kwargs):
+        picture = _get_picture(picture_id)
+        return render(request, self.template_name, {'picture': picture})
+
+
+class EditPictureView(TemplateView):
+    template_name = 'edit_picture.html'
+
+    def get(self, request, picture_id=-1, *args, **kwargs):
+        picture = _get_picture(picture_id)
+        form = PictureForm(instance=picture)
+        return render(request, self.template_name, {
+            'form': form,
+            'picture_id': picture_id,
+        })
+
+    def post(self, request, picture_id=-1, *args, **kwargs):
+        picture = _get_picture(picture_id)
+        form = PictureForm(request.POST, request.FILES, instance=picture)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'status': 'successful'})
+
+        return JsonResponse({
+            'status': 'failed',
+            'errors': form.errors
+        })
+
+
+def _get_picture(picture_id):
+    try:
+        picture = Picture.objects.get(pk=picture_id)
+        return picture
+    except Picture.DoesNotExist:
+        raise Http404("Picture not Found")
